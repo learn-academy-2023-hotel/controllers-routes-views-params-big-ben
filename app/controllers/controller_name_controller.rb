ba3e369770ad4cbf5f7ca38ce3f2bf894bb422e9
zeniavarela <zvarela8@gmail.com>
2023-11-17 14:22:12 -0800

    routes completed

---
+++ app/controllers/controller_name_controller.rb
@@ -0,0 +1,14 @@
+class ControllerNameController < ApplicationController
+
+  def landing
+  end
+
+  def member1 
+    @joseph = ['San Diego', 'Alameda', 'San Francisco']
+
+  end
+  def member2 
+    @zenia = ['Ruka', 'Chico', 'Bluey']
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/landing' => 'controller_name#landing'
+    root "controller_name#landing"
+  get '/member1' => 'controller_name#member1'
+  get '/member2' => 'controller_name#member2'
 
   # Defines the root path route ("/")
   # root "articles#index"
